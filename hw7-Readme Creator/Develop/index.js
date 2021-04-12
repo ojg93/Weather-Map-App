c2f5a8dd3722246ca18f808f1ab63d025ff165ee
Omari Grampus <ojgrampus@gmail.com>
2021-04-11 20:50:15 -0400

    added prompt questions

---
+++ hw7-Readme Creator/Develop/index.js
@@ -0,0 +1,44 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your project title?',
+      name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Give a brief desctiption of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What is your github username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+    },
+  ])
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
