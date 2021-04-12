f23a78a3eab4c07e78acd0c894c05aa9b919c742
Omari Grampus <ojgrampus@gmail.com>
2021-04-12 19:32:00 -0400

    started creating functions for readme

--- hw7-Readme Creator/Develop/index.js
+++ hw7-Readme Creator/Develop/index.js
@@ -1,8 +1,11 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [
-    inquirer
+inquirer
   .prompt([
     {
       type: 'input',
@@ -26,19 +29,47 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'What is your project title?',
-        name: 'title',
+        message: 'How do use your project? ',
+        name: 'usage',
     },
-  ])
-];
-
+    {
+      type: 'input',
+      message: 'Who contributed to your project? ',
+      name: 'contribution',
+      
+    },
+    {
+      type: 'input',
+      message: 'How do install your project? ',
+      name: 'installation',
+      
+    },
+    {
+      type: 'input',
+      message: 'How do you test your project? ',
+      name: 'test',
+      
+    }
+    ]).then((response) => 
+    title, description, username, email, usage, contribution, installation
+    )
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile("README.md", data ,function(err){
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+
+  })
     
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
+  
 
 // Function call to initialize app
-init();
+// init();
+generateMarkdown(data);
+writeToFile(fileName, data);
--- hw7-Readme Creator/Develop/utils/generateMarkdown.js
+++ hw7-Readme Creator/Develop/utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return ""
+  
 }
 
 // TODO: Create a function that returns the license link
@@ -16,11 +16,40 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-          ## ${data.username}
-          ## ${data.description}
-          ### ${data.email}
+  return `## *TABLE OF CONTENTS*
+            
+            - [Title](#title) 
+            - [Username](#username) 
+            - [Description](#description) 
+            - [Email](#email) 
+            - [installation](#installation)
+            - [Usage](#usage) 
+            - [Contribution](#contribution)  
+            - [Tests](#tests) 
 
+          ${data.title}
+          # title:
+          
+          ${data.username}
+          # username:
+          
+          ${data.description}
+          ## description:
+          
+          ${data.email}
+          ## email:
+        
+          ${data.test}
+          # test
+
+          ${data.contribution}
+          # contrbution:
+
+          ${data.usage}
+          # usage:
+          
+          ${data.installation}
+          # installation: 
 
 `;
 }
