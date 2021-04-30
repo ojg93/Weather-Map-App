6e3630b6a0e549dc4b994aee7e1b20185608ea08
Omari Grampus <ojgrampus@gmail.com>
2021-04-30 12:21:37 -0500

    complete POST and GET functions

--- hw9-Express Note Taker/index.js
+++ hw9-Express Note Taker/index.js
@@ -1,19 +1,36 @@
-console.log('llets get down to business')
+const fs = require('fs');
+const express = require("express");
+const path = require("path");
 
-const express = require('express')
-const path = require('path')
-
-const app = express()
-const PORT = 5500
+const app = express();
+const PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-
-app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.use(express.static("./public"));
 
 app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
-  });
-  
+  console.log(`App listening on PORT: ${PORT}`);
+});
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
+
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+
+ app.post('api/notes', (req, res) => {
+   const data = 
+    {   title:"",
+        text:""
+    }
+    data.req.body.
+    res.json(data)
+})
+
+ app.get('api/notes', (req, res) => res.readFile('/db.json', "uft8", function (err,data) {
+    if (err) {
+      return console.log(err);
+    } 
+    console.log(data);
+}));
+ 
